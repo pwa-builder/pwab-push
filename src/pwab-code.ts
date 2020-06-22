aff31dee1842472208460664e9f2cba0a3124721
lee-leonardo <lee.leonardo.king@gmail.com>
2020-06-22 12:05:34 -0700

    start the script tags in a separate file

---
+++ src/pwab-code.ts
@@ -0,0 +1,15 @@
+export const vanilla = `<script>
+  <h1>AA</h1>
+</script>`;
+
+export const react = `<script>
+  <h1>AA</h1>
+</script>`;
+
+export const angular = `<script>
+  <h1>AA</h1>
+</script>`;
+
+export const vue = `<script>
+  <h1>AA</h1>
+</script>`;
