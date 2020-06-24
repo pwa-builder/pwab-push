24410d990cbc07a7cdcd4b9fc15868e12e0125b8
lee-leonardo <lee.leonardo.king@gmail.com>
2020-06-23 23:53:17 -0700

    cleaned up script to subscribe the user... need to clean up above

--- src/pwab-code.ts
+++ src/pwab-code.ts
@@ -1,4 +1,21 @@
-export const vanilla = `<script>
+export const vanilla = `
+  self.addEventListener("push", (event) => {
+    const data = event.data.json();
+
+    event.waitUntil(
+      registration.showNotification(data.notification.title, data.notification)
+    );
+  });
+
+
+
+
+
+
+
+
+
+<script>
   var publicKey = "the cryptographic hash emailed from above!";
   var privateKey = "";
 
@@ -123,9 +140,7 @@ export const vanilla = `<script>
 
 </script>`;
 
-export const react = `<script>
-  react
-</script>`;
+export const react = vanilla;
 
 export const angular = `<script>
   angular
@@ -134,3 +149,68 @@ export const angular = `<script>
 export const vue = `<script>
   vue
 </script>`;
+
+export const subscribeUser = `
+// Add these functions to prompt the user to subscribe to the pwabuilder push notification server.
+async function subscribeUser() {
+  try {
+    const vapidPublicKey = ""; // get public key here
+    var registration = await navigator.serviceWorker.register("pwabuilder-sw.js", {
+      scope: "./"
+    });
+
+    var subscription = await registration.pushManager.getSubscription();
+
+    if (subscription) {
+      return;
+    }
+
+    const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+    subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: convertedVapidKey
+    });
+
+    const response = await fetch(pushServiceUrl + '?action=subscribe', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        subscription: JSON.stringify(subscription),
+        publicKey: vapidPublicKey
+      })
+    });
+
+    // handle response after registraction here
+
+  } catch(e) {
+    // handle error here
+  }
+}
+
+// Utility function for browser interoperability
+function urlBase64ToUint8Array(base64String) {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+`;
+
+export const landingScript = `
+// Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
+
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
+`;
