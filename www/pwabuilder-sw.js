1e204311a216256ad40cb6fd6400e08435d7937c
lee-leonardo <lee.leonardo.king@gmail.com>
2020-07-01 20:47:30 -0700

    responses being cached...

--- www/pwabuilder-sw.js
+++ www/pwabuilder-sw.js
@@ -11,55 +11,55 @@ self.addEventListener("install", function (event) {
   );
 });
 
-// If any fetch fails, it will show the offline page.
-self.addEventListener("fetch", function (event) {
-  console.log("fetch");
+// // If any fetch fails, it will show the offline page.
+// self.addEventListener("fetch", function (event) {
+//   console.log("fetch");
 
-  if (event.request.method !== "GET") return;
+//   if (event.request.method !== "GET") return;
 
-  event.respondWith(
-    fetch(event.request)
-      .then(function (res) {
-        // if (!res || res.status !== 200 || res.type !== 'basic') {
-        //   return res;
-        // }
+//   event.respondWith(
+//     // fetch(event.request)
+//     //   .then(function (res) {
+//     //     // if (!res || res.status !== 200 || res.type !== 'basic') {
+//     //     //   return res;
+//     //     // }
 
-        // var toCache = res.clone();
-        // caches.open(CACHE)
-        //   .then(function (cache) {
-        //     cache.put(evt.request, toCache);
-        //   });
+//     //     // var toCache = res.clone();
+//     //     // caches.open(CACHE)
+//     //     //   .then(function (cache) {
+//     //     //     cache.put(evt.request, toCache);
+//     //     //   });
 
-        return res;
-      })
-    // .catch(function (error) {
-    // The following validates that the request was for a navigation to a new document
-    // if (
-    //   event.request.destination !== "document" ||
-    //   event.request.mode !== "navigate"
-    // ) {
-    //   return;
-    // }
+//     //     return res;
+//     //   })
+//     // .catch(function (error) {
+//     // The following validates that the request was for a navigation to a new document
+//     // if (
+//     //   event.request.destination !== "document" ||
+//     //   event.request.mode !== "navigate"
+//     // ) {
+//     //   return;
+//     // }
 
-    // console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
-    // return caches.open(CACHE).then(function (cache) {
-    //   return cache.match(offlineFallbackPage);
-    // });
-    // })
-  );
+//     // console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
+//     // return caches.open(CACHE).then(function (cache) {
+//     //   return cache.match(offlineFallbackPage);
+//     // });
+//     // })
+//   );
 });
 
 // This is an event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener("refreshOffline", function () {
-  const offlinePageRequest = new Request(offlineFallbackPage);
+// self.addEventListener("refreshOffline", function () {
+//   const offlinePageRequest = new Request(offlineFallbackPage);
 
-  return fetch(offlineFallbackPage).then(function (response) {
-    return caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
-      return cache.put(offlinePageRequest, response);
-    });
-  });
-});
+//   return fetch(offlineFallbackPage).then(function (response) {
+//     return caches.open(CACHE).then(function (cache) {
+//       console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
+//       return cache.put(offlinePageRequest, response);
+//     });
+//   });
+// });
 
 // This is an event that can be fired by the browser to tell the SW to display a web push notification
 // Note: The user must have first agreed to subscribe to notifications
